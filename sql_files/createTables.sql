0d7da5b5c1c68d8cf11fc9f685c0e7a7357d6dc4
karthick <karthickpalanivel4@gmail.com>
2025-06-18 11:46:44 +0530

    Revert "altered sql variables"
    This reverts commit ec78ced8e8d72271203aa3e758f3a5fe5258db3b.

--- sql_files/createTables.sql
+++ sql_files/createTables.sql
@@ -30,7 +30,7 @@ CREATE TABLE orders (
     customer_id STRING,
     products STRING,
     total_order_amount DOUBLE,
-    order_date STRING,
+    date STRING,
     returned STRING
 )
 ROW FORMAT DELIMITED
